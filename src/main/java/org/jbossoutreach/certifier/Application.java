18fb32935e4194ae2974be708de7407f1030a17a
jainnipun <Nipun@Nipun-MacBook-Air.local>
2017-10-25 15:26:54 +0530

    Initial Commit

---
+++ src/main/java/org/jbossoutreach/certifier/Application.java
@@ -0,0 +1,44 @@
+package org.jbossoutreach.certifier;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerHelper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Application {
+    public static final String DEST = "~/new1.pdf";
+    public static final String SOURCE_HTML = "templates/basic.html";
+
+    public static void main(String[] args) throws IOException, DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new Application().createPdf(DEST);
+        System.out.println("------------------------"+file.getAbsolutePath());
+    }
+
+    /**
+     * Creates a PDF with the words "Hello World"
+     * @param file
+     * @throws IOException
+     * @throws DocumentException
+     */
+    public void createPdf(String file) throws IOException, DocumentException {
+        // step 1
+        Document document = new Document();
+        // step 2
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file));
+        // step 3
+        document.open();
+        // step 4
+        XMLWorkerHelper.getInstance().parseXHtml(writer, document,
+                Application.class.getResourceAsStream(SOURCE_HTML));
+        // step 5
+        document.close();
+    }
+
+}
