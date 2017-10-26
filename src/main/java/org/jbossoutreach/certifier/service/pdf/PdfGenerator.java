d6a870d9883092dbb8bc2e400f4d8d0b64948c33
Anuj Garg <anujgargcse@gmail.com>
2017-10-26 16:41:56 +0530

    create file chooser (#6)

--- src/main/java/org/jbossoutreach/certifier/service/pdf/PdfGenerator.java
+++ src/main/java/org/jbossoutreach/certifier/service/pdf/PdfGenerator.java
@@ -1,35 +1,30 @@
-package org.jbossoutreach.certifier;
+package org.jbossoutreach.certifier.service.pdf;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Element;
 import com.itextpdf.text.PageSize;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.tool.xml.ElementList;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
-import org.jbossoutreach.certifier.service.reader.CSVReader;
 
-import javax.swing.text.html.StyleSheet;
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-public class Application {
+public class PdfGenerator {
 
     // destination paths
     public static final String DEST = "./output/new1.pdf";
     public static final String SOURCE_HTML = "templates/basic.html";
 
-    public static void main(String[] args) throws IOException, DocumentException {
+        public static void main(String[] args) throws IOException, DocumentException {
         File file = new File(DEST);
         file.getParentFile().mkdirs();
-        new Application().createPdf(DEST);
+        new PdfGenerator().createPdf(DEST);
     }
 
     public void createPdf(String file) throws IOException, DocumentException {
 
-        CSVReader.read();
-/*        // step 1
+        // step 1
         Document document = new Document(PageSize.A4.rotate());
         // step 2
         PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file));
@@ -39,7 +34,7 @@ public class Application {
         XMLWorkerHelper.getInstance().parseXHtml(writer, document,
                 getClass().getClassLoader().getResourceAsStream(SOURCE_HTML));
         // step 5
-        document.close();*/
+        document.close();
     }
 
 }
