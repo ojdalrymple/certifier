c403b74e7d9997c1878bcb0d7ed5e070a605f112
jainnipun <nipunjain24x7@gmail.com>
2017-10-26 12:34:10 +0530

    HTML formatting fixed (#2)

--- src/main/java/org/jbossoutreach/certifier/Application.java
+++ src/main/java/org/jbossoutreach/certifier/Application.java
@@ -2,44 +2,39 @@ package org.jbossoutreach.certifier;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.ElementList;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import javax.swing.text.html.StyleSheet;
+import java.io.*;
 
 public class Application {
 
-    // is this now a better comment?
-
-    public static final String DEST = "~/new1.pdf";
+    // destination paths
+    public static final String DEST = "./output/new1.pdf";
     public static final String SOURCE_HTML = "templates/basic.html";
 
     public static void main(String[] args) throws IOException, DocumentException {
         File file = new File(DEST);
         file.getParentFile().mkdirs();
         new Application().createPdf(DEST);
-        System.out.println(file.getAbsolutePath());
     }
 
-    /**
-     * Creates a PDF with the words "Hello World"
-     * @param file
-     * @throws IOException
-     * @throws DocumentException
-     */
     public void createPdf(String file) throws IOException, DocumentException {
         // step 1
-        Document document = new Document();
+        Document document = new Document(PageSize.A4.rotate());
         // step 2
         PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file));
         // step 3
         document.open();
         // step 4
         XMLWorkerHelper.getInstance().parseXHtml(writer, document,
-                Application.class.getResourceAsStream(SOURCE_HTML));
+                getClass().getClassLoader().getResourceAsStream(SOURCE_HTML));
         // step 5
         document.close();
     }
