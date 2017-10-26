03143a64a6938bad69b0d3603dc09bc6ee7baa85
garg-anuj <anujgargcse@gmail.com>
2017-10-26 17:17:29 +0530

    Remove java fx

--- src/main/java/org/jbossoutreach/certifier/App.java
+++ src/main/java/org/jbossoutreach/certifier/App.java
@@ -1,57 +1,9 @@
 package org.jbossoutreach.certifier;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-
-import javafx.scene.control.Button;
-import java.io.File;
-import java.util.List;
-
-
-public class App extends Application {
-
-    @Override
-    public void start(final Stage stage) {
-        stage.setTitle("File Chooser Sample");
-
-        final FileChooser fileChooser = new FileChooser();
-
-        final Button openButton = new Button("Open a Csv...");
-
-        openButton.setOnAction( e -> {
-                    File file = fileChooser.showOpenDialog(stage);
-                    if (file != null) {
-                        openFile(file);
-                    }
-                });
-
-        final GridPane inputGridPane = new GridPane();
-
-        GridPane.setConstraints(openButton, 0, 0);
-        inputGridPane.setHgap(6);
-        inputGridPane.setVgap(6);
-        inputGridPane.getChildren().addAll(openButton);
-
-        final Pane rootGroup = new VBox(12);
-        rootGroup.getChildren().addAll(inputGridPane);
-
-        stage.setScene(new Scene(rootGroup));
-        stage.show();
-    }
+public class App {
 
     public static void main(String[] args) {
-        Application.launch(args);
-    }
-
-    private void openFile(File file) {
-        System.out.println(file.getAbsolutePath());
+        System.out.println("App is up and running");
     }
 
 }
